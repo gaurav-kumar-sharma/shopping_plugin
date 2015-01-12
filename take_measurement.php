38beb3c0a111a677ff68101ecfbb4d3636d1d89e
gaurav-kumar-sharma <gauravkumarsharma.work@gmail.com>
2015-01-12 08:26:58 +0530

    plugin added

---
+++ take_measurement.php
@@ -0,0 +1,109 @@
+<?php
+$gender = $_GET['gender'];
+$productId = $_GET['productId'];
+$gender = 'M';
+?>
+<html>
+	<head>
+		<script type="text/javascript" src='js/jQuery.min.js'></script>
+	</head>
+	<body>
+		<div border = '1' style= 'height:99%;width:99%;<!-- border: 1px solid black; -->'>
+			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'>
+				<div border = '1' style= 'height:32%;width:100%;float:left;border: 1px solid green;'>
+
+				</div>
+				<div border = '1' style= 'height:64%;width:100%;float:left;border: 1px solid blue;'>
+					<div>
+					
+						<div id='maleMeasurement' <?php if($gender == 'F') {?>style = 'display:none' <?php } ?>>
+							<label>HEIGHT</label>
+							<input type="text" id = 'height' name = 'height'><br/>
+
+							<label>NECK</label>
+							<input type="text" id = 'neck' name = 'neck'><br/>
+
+							<label>CHEST</label>
+							<input type="text" id = 'chest' name = 'chest'><br/>
+
+							<label>WAIST</label>
+							<input type="text" id = 'waist' name = 'waist'><br/>
+
+							<label>ARM</label>
+							<input type="text" id = 'arm' name = 'arm'>
+						</div>
+						<?php } else { ?>
+						<div id='femaleMeasurement' <?php if($gender == 'M') {?>style = 'display:none' <?php } ?>>
+							<label>HEIGHT</label>
+							<input type="text" id = 'height' name = 'height'><br/>
+
+							<label>NECK</label>
+							<input type="text" id = 'neck' name = 'neck'><br/>
+
+							<label>CHEST</label>
+							<input type="text" id = 'chest' name = 'chest'><br/>
+
+							<label>WAIST</label>
+							<input type="text" id = 'waist' name = 'waist'><br/>
+
+							<label>ARM</label>
+							<input type="text" id = 'arm' name = 'arm'>
+						</div>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'>
+				<div>
+					<div id="manAvtar" style="display: block;">
+				        <img src="images/avatar/man/stand.png" class="default" alt="" style="display: inline;position: absolute;height: 80%;">
+				        <img src="images/avatar/man/stand.png" class="height" alt="How to measure height" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/man/arm.png" class="arm" alt="How to measure arm" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/man/neck.png" class="neck" alt="How to measure neck" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/man/chest.png" class="chest" alt="How to measure chest" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/man/waist.png" class="waist" alt="How to measure waist" style="display: none;position: absolute;height: 80%;">
+				    </div>
+
+				    <div id="womanAvtar" style="display: block;">
+				        <img src="images/avatar/woman/stand.png" class="default" alt="" style="display: inline;position: absolute;height: 80%;">
+				        <img src="images/avatar/woman/stand.png" class="height" alt="How to measure height" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/woman/arm.png" class="arm" alt="How to measure arm" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/woman/neck.png" class="neck" alt="How to measure neck" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/woman/chest.png" class="chest" alt="How to measure chest" style="display: none;position: absolute;height: 80%;">
+				        <img src="images/avatar/woman/waist.png" class="waist" alt="How to measure waist" style="display: none;position: absolute;height: 80%;">
+				    </div>
+				</div>
+			</div>
+			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'></div>
+		</div>
+
+	</body>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			var measureName;
+			console.log($("div#maleMeasurement").find("input"));
+			$("div#maleMeasurement").find("input").focus(function(){
+				measureName = $(this).attr('id');
+				$("#manAvtar").find("img").hide();
+				$("#manAvtar").find("img."+measureName).show();
+			});
+			$("div#maleMeasurement").find("input").blur(function(){
+				$("#manAvtar").find("img").hide();
+				$("#manAvtar").find("img.default").show();
+			});
+
+			$("div#femaleMeasurement").find("input").focus(function(){
+				measureName = $(this).attr('id');
+				$("#womanAvtar").find("img").hide();
+				$("#womanAvtar").find("img."+measureName).show();
+			});
+			$("div#femaleMeasurement").find("input").blur(function(){
+				$("#womanAvtar").find("img").hide();
+				$("#womanAvtar").find("img.default").show();
+			});
+
+		});
+	</script>
+</html>
+
+
