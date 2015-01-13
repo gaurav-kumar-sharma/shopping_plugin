5a6838c1d8ee39791ca5f8e0aaa04f85dcb69755
gauravsharma <gaurav.sharma@osscube.com>
2015-01-13 18:52:38 +0530

    worked on Virtual me

--- take_measurement.php
+++ take_measurement.php
@@ -7,11 +7,20 @@ $gender = $_GET['gender'];
 	<head>
 		<script type="text/javascript" src='js/jQuery.min.js'></script>
 		<style>
-			
+			input {
+				width :20px;
+			}
 		</style>
+		
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+    <style type="text/css">
+    #slider { margin: 10px; }
+  </style>
 	</head>
 	<body>
-		<div border = '1' style= 'height:99%;width:99%;<!-- border: 1px solid black; -->'>
+		<div border = '1' id= 'measurementContent' style= 'height:99%;width:99%;<!-- border: 1px solid black; -->'>
 			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'>
 				<div border = '1' style= 'height:22%;width:100%;float:left;border: 1px solid green;'>
 
@@ -54,7 +63,7 @@ $gender = $_GET['gender'];
 					</div>
 				</div>
 				<div>
-						<input type = 'button' value = 'Show Virtual Me '>
+						<input type = 'button' value = 'Show Virtual Me ' id = 'virtualBtn'>
 				</div>
 			</div>
 			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'>
@@ -78,8 +87,39 @@ $gender = $_GET['gender'];
 				    </div>
 				</div>
 			</div>
-			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'></div>
+			<div  border = '1' style= 'height:97%;width:32%;float:left;border: 1px solid red;'>
+				 <p>
+  The jQuery UI Slider plugin makes selected elements into sliders. There are various options such as multiple handles, and ranges. The handle can be moved with the mouse or the arrow keys.
+  </p>
+  <p>
+  This sample demonstrates the simple usage of the slider seek to function.
+  For more information about slider, please proceed to http://docs.jquery.com/UI/Slider
+  </p>
+<div id="slider"></div>
+Seek To : <input id="seekTo1" type="text" value="10" /> <input id="seekTo2" type="text" value="50" />
+<input id="update" type="button" value="Update" />
+Current Value : <span id="val">0 - 50</span>
+			</div>
 		</div>
+		<div id = 'virtualMe' style= 'height:99%;width:99%;border: 1px solid black;'>
+			<div style = 'position: relative;z-index: 211;top: 73px;left: 70px;'>
+				<img src = 'images/garments-line.png' style = 'position: relative;z-index:111;left:5px' >	
+				<div id = hangedProd style= 'position: relative;left: -25px;'>
+					<div style = 'top:-30px;position: relative;z-index : 111;float:left'>
+<!-- 						<label>30</label> -->
+						<div>30</div>
+						<img src = 'images/hanger-left-prod.png' style = 'height: 70%;'>
+					</div>
+					<div style = 'top:-20px;position: relative;z-index : 211;left: -150px;float:left'>
+						<img src = 'images/hanger-left-prod.png' style = 'height: 70%;'>
+					</div>
+					<div style = 'top:-10px;position: relative;z-index : 311;left: -300px;float:left'>
+						<img src = 'images/hanger-left-prod.png' style = 'height: 70%;'>
+					</div>
+				</div>
+			</div>	
+		</div>
+		
 
 	</body>
 	<script type="text/javascript">
@@ -106,8 +146,31 @@ $gender = $_GET['gender'];
 				$("#womanAvtar").find("img.default").show();
 			});
 
+          $("#slider").slider(
+          {
+              range: true,
+              min: 20,
+              max: 100,
+              step: 1,
+              values: [30, 50],
+              change: showValue
+          });
+          $("#update").click(function () {
+              $("#slider").slider("option", "values", [$("#seekTo1").val(), $("#seekTo2").val()]);
+
+          });
+          function showValue(event, ui) {
+              $("#val").html(ui.values[0] + " - " + ui.values[1]);
+          }
+
+          $("#virtualBtn").click(function(){
+              $("#measurementContent").hide();
+              $("#virtualMe").show();
+
+          });
+		      
+
 		});
 	</script>
 </html>
-
 
